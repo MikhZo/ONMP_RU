ce93fb6a25034b1d5233fb65ea15192c2792b1d8
Mikhaelle <42121520+MikhZo@users.noreply.github.com>
2025-04-04 02:42:32 +0300

    Update oneclick_ru.sh
    bugfix

--- oneclick_ru.sh
+++ oneclick_ru.sh
@@ -37,7 +37,7 @@ Install()
 	mkdir -p /opt/onmp
 
     # Получение скрипта ONMP
-    curl -kfsSL https://raw.githubusercontent.com/MikhZo/ONMP-RU/master/onmp_ru.sh > /opt/onmp/onmp.sh
+    curl -kfsSL https://raw.githubusercontent.com/MikhZo/ONMP_RU/master/onmp_ru.sh > /opt/onmp/onmp.sh
     # curl -kfsSL http://192.168.4.126:4000/onmp.sh > /opt/onmp/onmp.sh
     chmod +x /opt/onmp/onmp.sh
 
@@ -50,7 +50,7 @@ Install()
 Update()
 {
 	rm -rf /opt/onmp/onmp.sh
-	curl -kfsSL https://raw.githubusercontent.com/MikhZo/ONMP-RU/master/onmp_ru.sh > /opt/onmp/onmp.sh
+	curl -kfsSL https://raw.githubusercontent.com/MikhZo/ONMP_RU/master/onmp_ru.sh > /opt/onmp/onmp.sh
 	# curl -kfsSL http://192.168.4.126:4000/onmp.sh > /opt/onmp/onmp.sh
 	chmod +x /opt/onmp/onmp.sh
 	/opt/onmp/onmp.sh renewsh > /dev/null 2>&1
